328d27966a814ceb395a67189ac03d5fd7343d29
hayle778 <hayjclove@gmail.com>
2019-10-22 15:49:17 -0400

    BS

---
+++ BS/src/Base.java
@@ -0,0 +1,52 @@
+/* 1.print booelean y first
+   2.then print int x  = 400;
+
+*/
+public class Base {
+    public static void main(String[] args) {
+        int x = 400;
+        boolean y = false;
+        char m = 'T';
+        double k = 1.0;
+        //System.out.println(y = true);
+
+
+        System.out.println(y);
+
+
+
+       System.out.println(x);
+       System.out.println(m);
+       System.out.println(k);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
